be1b9908efae544b9bc23eab2d8079d4d1f94201
MoskvinaAlexandra <Moskvina.Alexandra@urfu.me>
2026-03-03 17:29:48 +0500

    123

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const todos = findTodos()
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -11,10 +12,52 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function findTodos(){
+    arr = [];
+    for(const file of files){
+        let pos = 0;
+        const target = '// ' + 'TODO'
+        while (true) {
+            let foundPos = file.indexOf(target, pos);
+
+            if (foundPos == -1) 
+                break;
+
+            let endPos = file.indexOf('\n', foundPos + 1);
+            endPos = endPos === -1 ? file.length: endPos;
+            arr.push(file.substring(foundPos, endPos));
+            pos = foundPos + 1;
+        }
+    }
+    return arr;
+}
+
+function findUser(user){
+
+}
+
+function show(){
+    for(let todo of todos)
+        console.log(todo);
+}
+
+function important(){
+    for(let todo of todos){
+        if(todo.indexOf('!') != -1)
+            console.log(todo);
+    }
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            show();
+            break;
+        case 'important':
+            important();
             break;
         default:
             console.log('wrong command');
